9d664a5a5174a1f16c03338969029c2daf3abee8
Javier Murillo Romero <javim552@gmail.com>
2022-10-07 15:34:18 +0000

    Generar una carta poker aleatoria

--- src/app.js
+++ src/app.js
@@ -5,7 +5,19 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+const numero = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const iconos = ["❤️", " ♠️", "♣️", "♦️"];
+
+function randomcartaselement(array) {
+  const numeroaleatorio = Math.floor(Math.random() * array.length);
+  const elementoaleatorio = array[numeroaleatorio];
+  return elementoaleatorio;
+}
+
+const numerocarta = document.querySelector("#numero");
+const iconocarta = document.querySelector("#iconos1");
+const iconocarta1 = document.querySelector("#iconos2");
+numerocarta.innerHTML = randomcartaselement(numero);
+let finaliconos = randomcartaselement(iconos);
+iconocarta.innerHTML = finaliconos;
+iconocarta1.innerHTML = finaliconos;
